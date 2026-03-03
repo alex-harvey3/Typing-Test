ca0e35e0d50b4bb83093f6c3ee5ad22c9f890834
alexharvey-3 <alexjharv03@gmail.com>
2026-03-02 21:07:24 -0600

    Created the start screen function.

--- typing_test.py
+++ typing_test.py
@@ -1,6 +1,13 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the typing test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+    
 def main(stdscr):
     # The pairing of a foreground color of green and background of black stored as 1
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -9,11 +16,8 @@ def main(stdscr):
     # Foreground color white, background color black, stored as 3
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
-    
-    stdscr.clear()
-    stdscr.addstr("Hello World!")
-    stdscr.refresh()
-    stdscr.getkey()
+    # Call the start screen function
+    start_screen(stdscr)
  
 # Intitializes curses when called   
 wrapper(main)
