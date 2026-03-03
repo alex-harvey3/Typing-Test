6d22bac16cdfbc0d02a988bca0298691e49c77b5
alexharvey-3 <alexjharv03@gmail.com>
2026-03-02 21:25:52 -0600

    Added backspace functionality.

--- typing_test.py
+++ typing_test.py
@@ -8,6 +8,42 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
     
+def wpm_test(stdscr):
+    target_text = "This is some test text!"
+    # List to store what the user types
+    current_text = []
+    
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    
+    while True:
+        # Screen needs to be constantly clear to avoid writing words on top of each other
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        
+        # Display every character the user has typed as green
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+        stdscr.refresh()
+        
+        
+        # Store the key that the user types and add to list
+        key = stdscr.getkey()
+        
+        # Exit the app if the escape key is pressed
+        if ord(key) == 27:
+            break
+        
+        
+        # Ensure that backspace is handled correctly
+        if key in ("KEY_BACKSPACE", "\b", "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+        
+        
+
 def main(stdscr):
     # The pairing of a foreground color of green and background of black stored as 1
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -18,6 +54,8 @@ def main(stdscr):
     
     # Call the start screen function
     start_screen(stdscr)
+    # Call the test function
+    wpm_test(stdscr)
  
 # Intitializes curses when called   
 wrapper(main)
