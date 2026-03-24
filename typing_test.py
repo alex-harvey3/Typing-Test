c770039437aca15e28c8e920bdf72e87110bc1c4
alexharvey-3 <alexjharv03@gmail.com>
2026-03-23 20:42:58 -0500

    Added accuracy tracker.

--- typing_test.py
+++ typing_test.py
@@ -26,13 +26,19 @@ def start_screen(stdscr):
     return target_text
         
     
-def display_text(stdscr, target, current, wpm=0):
+def display_text(stdscr, target, current, wpm=0, accuracy=0):
     stdscr.addstr(target)
     # Display words per minute one line under the target text
     stdscr.addstr(4, 0, f"WPM: {wpm}")
+    # Display accuracy
+    stdscr.addstr(5, 0, f"Accuracy: {accuracy}")
     height, width = stdscr.getmaxyx()
+    
+    # Initialize counter variables. character counters have to start at 1 to avoid divide by zero error
     increment = 0
-        
+    correct_num = 1
+    total_chars = 1
+    
     # Display every character the user has typed directly on top of the target text
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -40,14 +46,17 @@ def display_text(stdscr, target, current, wpm=0):
         # Display the text as red if incorrect
         if char != correct_char:
             color = curses.color_pair(2)
-        print("i: ", i)
-        print("width: ", width)
+            total_chars +=1
         if i >= width:
             stdscr.addstr(1, i - (i-increment), char, color)
-            increment = increment + 1
+            increment += 1
         else:
             stdscr.addstr(0, i, char, color)
-    
+            correct_num += 1
+            total_chars += 1
+    return correct_num, total_chars
+
+
 def wpm_test(stdscr, target_text):
     # List to store what the user types
     current_text = []
@@ -55,18 +64,21 @@ def wpm_test(stdscr, target_text):
     # Start tracking time
     start_time = time.time()
     stdscr.nodelay(True)
+    correct = 1
+    total = 1
     
     while True:
         time_elapsed = max(time.time() - start_time, 1)
         # Assuming an average word length of 5 characters
         wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+        accuracy = (correct / total) * 100
         # Screen needs to be constantly clear to avoid writing words on top of each other
         stdscr.clear()
-        display_text(stdscr, target_text, current_text, wpm)
+        correct, total = display_text(stdscr, target_text, current_text, wpm, accuracy)
         stdscr.refresh()
         
         # Check if user has finished. ONly ends if everything typed is correct.
-        if "".join(current_text) == target_text:
+        if len("".join(current_text)) == len(target_text):
             stdscr.nodelay(False)
             break
         
@@ -98,9 +110,9 @@ def main(stdscr):
     # Foreground color white, background color black, stored as 3
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
-    # Call the start screen function
-    target = start_screen(stdscr)
     while True:
+        # Call the start screen function
+        target = start_screen(stdscr)
         # Call the test function
         wpm_test(stdscr, target)
         
